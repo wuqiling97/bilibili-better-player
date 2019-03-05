59b44ab55a25e3b426d3520f31520ba6596c72da
吴启凌 <wuqiling97@163.com>
2019-03-05 23:38:29 +0800

    调整代码结构

--- better_player.js
+++ better_player.js
@@ -14,12 +14,16 @@
 
 var $c = document.querySelector.bind(document);
 window.$c = $c;
-
+// 辅助函数
 Math.clamp = function(val, l, h) {
     (val < l) && (val = l);
     (val > h) && (val = h);
     return val;
 };
+function isclose(a, b) {
+    var min = Math.min(a, b);
+    return Math.abs(a-b) < min * 1e-7;
+}
 
 function log(...data) {
     console.log('[TAMPER]', ...data);
@@ -34,34 +38,32 @@ function getDanmuFontsize() {
     var s = JSON.parse(localStorage.bilibili_player_settings);
     return s.setting_config.fontsize;
 }
-
-function isint(n) {
-    return parseFloat(n.toFixed(5)) === Math.round(n);
-}
-function isclose(a, b) {
-    var min = Math.min(a, b);
-    return Math.abs(a-b) < min * 1e-7;
-}
+window.setDanmuFontsize = setDanmuFontsize;
+window.getDanmuFontsize = getDanmuFontsize;
 
 function isBigscreen() {
     return screen.width > 1500;
 }
 
+// 对付延迟加载
 function conditionExec(condition, task, timeout) {
     if(!condition()) {
         // log('wait', timeout, 'for', task);
         setTimeout(() => conditionExec(condition, task, timeout), timeout);
-        return;
+    } else {
+        task();
     }
-    task();
 }
+// 辅助函数end
 
 var isNew = !!$c('.has-stardust');
+// var isNew = getCookie('stardustvideo') == '1';
 // location: url相关
 var isBangumi = location.pathname.indexOf('bangumi') !== -1;
 var isWatchlater = location.pathname.indexOf('watchlater') !== -1;
 var isNormal = !(isBangumi || isWatchlater);
 var noNewplayer = isWatchlater;
+
 
 // 字号函数
 var userFontSize = 0.7;
@@ -70,6 +72,7 @@ var ratio = userFontSize / newplayerFontSize;
 
 var correctSize = [18, 25, 36].map(x => userFontSize*x);
 var gmutationsList = [];
+// 检查元素字号是否正确
 function isSizeCorrect(element) {
     var ori = parseFloat(element.style.fontSize.replace('px', ''));
     if(!correctSize.map(x => isclose(x, ori)).reduce((x, y) => x||y)) {
@@ -161,10 +164,117 @@ function listenVideoPartChange() {
 }
 // 字号函数end
 
+
+function setSizeNormal() {
+    var isWide = window.isWide
+        , rcon_w = 350 // 右侧栏+左右栏margin 320+30
+        , height = $(window).height()
+        , width = $(window).width()
+        , thh = Math.round(16 * (height*0.68) / 9) // 用height限制宽度
+        , thw = width - 152 - rcon_w // 用width限制，实为margin+iswide限制
+        , videoW = Math.min(thw, thh);
+    videoW = Math.clamp(videoW, 638, 1280);
+    var w = videoW + rcon_w
+        , h = Math.round((videoW + (isWide ? rcon_w : 0)) * (9 / 16)) + 46
+        , pad = "0 " + (width < w + 152 ? 76 : 0) // margin至少76
+        , u = $c(".stardust-video .bili-wrapper")
+        , vwrap = $c(".v-wrap")
+        , bofqi = $c("#bofqi")
+        , dmbox = $c("#danmukuBox")
+        , pwrap = $c("#playerWrap");
+    u && (u.style.width = w + "px", u.style.padding = pad + "px");
+    vwrap && (vwrap.style.width = w + "px", vwrap.style.padding = pad + "px");
+    bofqi && (bofqi.style.width = w - (isWide ? 0 : rcon_w) + "px", bofqi.style.height = h + "px");
+    isWide ? (
+        dmbox && (dmbox.style.height = h - 0 + "px"),
+        pwrap && (pwrap.style.height = h - 0 + "px"),
+        bofqi && (bofqi.style.position = "absolute")
+    ) : (
+        dmbox && (dmbox.style.height = "auto"),
+        pwrap && (pwrap.style.height = "auto"),
+        bofqi && (bofqi.style.position = "static")
+    );
+    $c('.l-con').style.width = 'auto';
+    $c('.bilibili-player-video-danmaku').style.heigth = h-46-10 + 'px';
+}
+
+function setSizeBangumi() {
+    var maxW = md.specialCover ? 1070 : 1280
+    , rcon_w = 350
+    , height = $(window).height()
+    , width = $(window).width()
+    , thh = Math.round(md.specialCover ? 16 * (height - 264) / 9 - rcon_w : 16 * (0.68 * height) / 9)
+    , thw = width - 152 - rcon_w
+    , videoW = thw < thh ? thw : thh;
+    videoW < 638 && (videoW = 638),
+    maxW < videoW && (videoW = maxW);
+    var appWidth = videoW + rcon_w
+    , overflow = width < appWidth + 152;
+
+    $(".main-container").css({
+        width: overflow ? appWidth + 76 : appWidth,
+        paddingLeft: (overflow ? 76 : 0) + "px",
+        marginLeft: overflow ? "0" : "",
+        marginRight: overflow ? "0" : ""
+    })
+    if (md.specialCover) {
+        var _ = Math.round(9 * appWidth / 16 + 46);
+        $("#player_module").css({
+            height: _,
+            width: appWidth,
+            paddingLeft: "",
+            left: overflow ? 76 : "",
+            transform: overflow ? "none" : "",
+            webkitTransform: overflow ? "none" : ""
+        }),
+        $(".special-cover").css({
+            height: _ + 218
+        }),
+        $(".plp-l").css({
+            paddingTop: _ + 24
+        }),
+        $(".plp-r").css({
+            marginTop: _ + 40
+        }),
+        $("#danmukuBox").css({
+            top: -(_ + 40)
+        })
+    } else {
+        var p = Math.round(9/16 * (videoW + (window.isWide ? rcon_w : 0))) + 46 + (window.hasBlackSide && !window.isWide ? 96 : 0);
+        $("#danmukuBox").css({
+            top: ""
+        }),
+        window.isWide ? ($("#player_module").css({
+            height: p - 0,
+            width: "",
+            paddingLeft: overflow ? 76 : "",
+            left: "",
+            transform: "",
+            webkitTransform: ""
+        }),
+        $(".plp-l").css({
+            paddingTop: p - 0
+        }),
+        $(".plp-r").css({
+            marginTop: p + 16
+        })) : ($("#player_module").css({
+            height: p - 0,
+            width: "",
+            paddingLeft: "",
+            left: "",
+            transform: "",
+            webkitTransform: ""
+        }),
+        $(".plp-l, .plp-r").removeAttr("style"))
+    }
+}
+
+
+/// 改播放器代码
 if(isNew && !noNewplayer) {
     // 新版
     /// 自定义字号
-    log('fontsize', getDanmuFontsize());
+    log('origin fontsize', getDanmuFontsize());
     conditionExec(
         () => $c('.bilibili-player-video-danmaku') && $c('.bilibili-player-video'),
         () => {fontSizeMain(); listenVideoPartChange();},
@@ -173,38 +283,7 @@ if(isNew && !noNewplayer) {
 
     /// 扩大新版播放器，滚动旧版播放器到适合观看的位置
     if(isNormal) {
-        window.setSize = function() {
-            var isWide = window.isWide
-                , rcon_w = 350 // 右侧栏+左右栏margin 320+30
-                , height = $(window).height()
-                , width = $(window).width()
-                , thh = Math.round(16 * (height*0.68) / 9) // 用height限制宽度
-                , thw = width - 152 - rcon_w // 用width限制，实为margin+iswide限制
-                , videoW = Math.min(thw, thh);
-            videoW = Math.clamp(videoW, 638, 1280);
-            var w = videoW + rcon_w
-                , h = Math.round((videoW + (isWide ? rcon_w : 0)) * (9 / 16)) + 46
-                , pad = "0 " + (width < w + 152 ? 76 : 0) // margin至少76
-                , u = $c(".stardust-video .bili-wrapper")
-                , vwrap = $c(".v-wrap")
-                , bofqi = $c("#bofqi")
-                , dmbox = $c("#danmukuBox")
-                , pwrap = $c("#playerWrap");
-            u && (u.style.width = w + "px", u.style.padding = pad + "px");
-            vwrap && (vwrap.style.width = w + "px", vwrap.style.padding = pad + "px");
-            bofqi && (bofqi.style.width = w - (isWide ? 0 : rcon_w) + "px", bofqi.style.height = h + "px");
-            isWide ? (
-                dmbox && (dmbox.style.height = h - 0 + "px"),
-                pwrap && (pwrap.style.height = h - 0 + "px"),
-                bofqi && (bofqi.style.position = "absolute")
-            ) : (
-                dmbox && (dmbox.style.height = "auto"),
-                pwrap && (pwrap.style.height = "auto"),
-                bofqi && (bofqi.style.position = "static")
-            );
-            $c('.l-con').style.width = 'auto';
-            $c('.bilibili-player-video-danmaku').style.heigth = h-46-10 + 'px';
-        } // setSize
+        window.setSize = setSizeNormal; // setSize
         setSize();
 
         // 0.24.2 调整元素顺序
@@ -224,76 +303,7 @@ if(isNew && !noNewplayer) {
         );
 
     } else if(isBangumi) {
-        window.setSize = function() {
-            var maxW = md.specialCover ? 1070 : 1280
-            , rcon_w = 350
-            , height = $(window).height()
-            , width = $(window).width()
-            , thh = Math.round(md.specialCover ? 16 * (height - 264) / 9 - rcon_w : 16 * (0.68 * height) / 9)
-            , thw = width - 152 - rcon_w
-            , videoW = thw < thh ? thw : thh;
-            videoW < 638 && (videoW = 638),
-            maxW < videoW && (videoW = maxW);
-            var appWidth = videoW + rcon_w
-            , overflow = width < appWidth + 152;
-
-            $(".main-container").css({
-                width: overflow ? appWidth + 76 : appWidth,
-                paddingLeft: (overflow ? 76 : 0) + "px",
-                marginLeft: overflow ? "0" : "",
-                marginRight: overflow ? "0" : ""
-            })
-            if (md.specialCover) {
-                var _ = Math.round(9 * appWidth / 16 + 46);
-                $("#player_module").css({
-                    height: _,
-                    width: appWidth,
-                    paddingLeft: "",
-                    left: overflow ? 76 : "",
-                    transform: overflow ? "none" : "",
-                    webkitTransform: overflow ? "none" : ""
-                }),
-                $(".special-cover").css({
-                    height: _ + 218
-                }),
-                $(".plp-l").css({
-                    paddingTop: _ + 24
-                }),
-                $(".plp-r").css({
-                    marginTop: _ + 40
-                }),
-                $("#danmukuBox").css({
-                    top: -(_ + 40)
-                })
-            } else {
-                var p = Math.round(9/16 * (videoW + (window.isWide ? rcon_w : 0))) + 46 + (window.hasBlackSide && !window.isWide ? 96 : 0);
-                $("#danmukuBox").css({
-                    top: ""
-                }),
-                window.isWide ? ($("#player_module").css({
-                    height: p - 0,
-                    width: "",
-                    paddingLeft: overflow ? 76 : "",
-                    left: "",
-                    transform: "",
-                    webkitTransform: ""
-                }),
-                $(".plp-l").css({
-                    paddingTop: p - 0
-                }),
-                $(".plp-r").css({
-                    marginTop: p + 16
-                })) : ($("#player_module").css({
-                    height: p - 0,
-                    width: "",
-                    paddingLeft: "",
-                    left: "",
-                    transform: "",
-                    webkitTransform: ""
-                }),
-                $(".plp-l, .plp-r").removeAttr("style"))
-            }
-        } // setSize
+        window.setSize = setSizeBangumi; // setSize
         setSize();
         $('#app').css('margin-top', '10px');
     }
